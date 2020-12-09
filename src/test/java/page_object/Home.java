0febf4fb7daa9ffaa930d0cfc1c17a9be18090e1
raihan <rayanmondo@gmail.com>
2020-12-08 21:08:31 -0500

    add all files

---
+++ src/test/java/page_object/Home.java
@@ -0,0 +1,12 @@
+package page_object;
+
+import org.openqa.selenium.WebDriver;
+
+public class Home {
+    WebDriver driver;
+    public Home(WebDriver driver) {
+        this.driver=driver;
+
+    }
+
+}
---
+++ src/test/java/page_object/NavigationBar.java
@@ -0,0 +1,30 @@
+package page_object;
+
+import command_providers.ActOn;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import seleniumAutomation.EbayPriceValidation;
+
+public class NavigationBar {
+    private final Logger LOGGER= LogManager.getLogger(NavigationBar.class);
+    private final By moseHover = By.xpath("//*[@id='mainContent']/div[1]/ul/li[3]/a[text()='Motors']");
+    private final By ClickOnCarsTruck=By.xpath("//*[@id='mainContent']/div//a[text()='Cars & Trucks']");
+
+    WebDriver driver;
+
+    public NavigationBar(WebDriver driver) {
+        this.driver = driver;
+    }
+    public NavigationBar mouseHover() {
+        ActOn.element(driver, moseHover).mouseHover();
+        return this;
+    }
+
+    public MakeSelect clickLink() {
+        ActOn.element(driver, ClickOnCarsTruck).click();
+        return new MakeSelect(driver);
+    }
+
+}
